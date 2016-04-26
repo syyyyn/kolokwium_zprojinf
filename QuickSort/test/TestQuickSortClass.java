b6e26b7ffc91ab73875408467301aa2a3bc66b2e
syyyyn <msynowiec@me.com>
2016-04-26 17:27:00 +0200

    aaa
    aaa

--- QuickSort/test/TestQuickSortClass.java
+++ QuickSort/test/TestQuickSortClass.java
@@ -12,7 +12,7 @@ public class TestQuickSortClass {
     @Test
     public void testSort()
     {
-        
+        int i = 0;
         QuickSort qs = new QuickSort();
         int[] inputArray = {9,7,5,11,12,2,14,3,10,6};
         int[] expectedArraySorted = {2,3,5,6,7,9,10,11,12,14};
